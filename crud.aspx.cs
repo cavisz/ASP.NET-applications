44e9b4c6f48215ca4f88126a5fa95886c037743f
cavisz <92187904+cavisz@users.noreply.github.com>
2021-11-08 17:01:59 -0300

    Update crud.aspx.cs

--- crud.aspx.cs
+++ crud.aspx.cs
@@ -1,13 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using MySql.Data.MySqlClient;
-using System.Data;
-using System.Configuration;
-
 public partial class Default2 : System.Web.UI.Page
 {
     MySqlConnection conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["YourConnection"].ConnectionString);
@@ -15,10 +6,11 @@ public partial class Default2 : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         string texto = "SELECT pessoal_c.PESS_STR_NOME, " +
-          " pessoal_c.PESS_STR_EMAIL," +
-          " pessoal_c.PESS_STR_TELEFONE" +
-          " FROM pessoal_c" +
-          " ORDER BY pessoal_c.PESS_STR_NOME ASC";
+      " pessoal_.c.PESS_INT_CODIGO," +
+      " pessoal_c.PESS_STR_EMAIL," +
+      " pessoal_c.PESS_STR_TELEFONE" +
+      " FROM pessoal_c" +
+      " ORDER BY pessoal_c.PESS_STR_NOME ASC";
 
         try
         {
@@ -38,7 +30,6 @@ public partial class Default2 : System.Web.UI.Page
             conexao.Close();
         }
     }
-
     protected void Button1(object sender, EventArgs e)
     {
 
@@ -67,45 +58,50 @@ public partial class Default2 : System.Web.UI.Page
         }
     }
 
-    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+
+    protected void OnRowCommand(object sender, GridViewCommandEventArgs e)
     {
+        string commandName = e.CommandName;
 
-
+        if (commandName == "Editar")
+        {
+           
+           
+        }
     }
-}
-
-protected void bnt_Editar(object sender, GridViewEditEventArgs e)
-{
-    MySqlConnection conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["YourConnection"].ConnectionString);
-
-    string texto3 = "SELECT pessoal_c.PESS_STR_NOME, " +
-          " pessoal_c.PESS_STR_EMAIL," +
-          " pessoal_c.PESS_STR_TELEFONE" +
-          " FROM pessoal_c";
-
-    try
+    protected void pesquisa(object sender, EventArgs e)
     {
-        conexao.Open();
+        string texto = "SELECT pessoal_c.PESS_STR_NOME, " +
+             " pessoal_.c.PESS_INT_CODIGO," +
+             " pessoal_c.PESS_STR_EMAIL," +
+             " pessoal_c.PESS_STR_TELEFONE" +
+             " FROM pessoal_c" +
+             " WHERE pessoal_.c.PESS_INT_CODIGO LIKE @Id" +
+             " ORDER BY pessoal_c.PESS_STR_NOME ASC";
 
-        MySqlCommand cmd = new MySqlCommand(texto3, conexao);
 
-        cmd.CommandText = "INSERT INTO pessoal_c VALUES (@Nome, @Telefone, @Email)";
+        try
+        {
+            conexao.Open();
+            MySqlCommand cmd = new MySqlCommand(texto, conexao);
+            cmd.Parameters.AddWithValue("@Id", CommandArgument.ToString());
 
-        cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
-        cmd.Parameters.AddWithValue("@Telefone", txtTelefone.Text);
-        cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            cmd.ExecuteNonQuery();
 
-        cmd.ExecuteNonQuery();
-        cmd.Dispose();
-        //GridView1.EditIndex = -1;
-        //BindGridView(); btnUpdate.Visible = false;
-    }
-    catch (MySqlException ex)
-    {
 
-    }
-    finally
-    {
-        conexao.Close();
+
+        }
+
+        catch (MySqlException ex)
+        {
+        }
+
+        finally
+        {
+            conexao.Close();
+        }
+
+
     }
 }
